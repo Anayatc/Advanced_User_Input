2c701c29dd2aad4824a43ca85d944451ae2925a4
anayatc@gmail.com <anayatc@sky.com>
2017-05-03 21:35:05 +0100

    advanced user input

--- Skeleton/tests/ex48_tests.py
+++ Skeleton/tests/ex48_tests.py
@@ -8,3 +8,14 @@ def test_directions():
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'),
                           ('direction', 'east')])
+
+
+def test_verbs():
+    assert_equal(lexicon.scan("go"), [('verb', 'go')])
+    result = lexicon.scan("go kill eat open")
+    assert_equal(result, [('verb', 'go'),
+                          ('verb', 'kill'),
+                          ('verb', 'eat'),
+                          ('verb', 'open')])
+
+
