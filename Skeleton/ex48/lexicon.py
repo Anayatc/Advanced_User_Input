f744966463f25397f5967ae8fd320f930e427e48
anayatc@gmail.com <anayatc@sky.com>
2017-05-03 21:29:09 +0100

    advanced user input

--- Skeleton/ex48/lexicon.py
+++ Skeleton/ex48/lexicon.py
@@ -13,3 +13,14 @@ def get_tuple(word):
         return ('verb', lowercased)
     elif lowercased in stop_words:
         return ('stop', lowercased)
+    elif lowercased in nouns:
+        return ('noun', lowercased)
+    elif lowercased.isdigit():
+        return ('number', int(lowercased))
+    else:
+        return ('error', word)
+
+
+def scan(sentence):
+    words = sentence.split()
+    return [get_tuple(word) for word in words]
