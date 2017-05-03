9a9c85f9a981e2c577a4420e9ce84d7aa3efcfa6
anayatc@gmail.com <anayatc@sky.com>
2017-05-03 21:28:28 +0100

    advanced user input

--- Skeleton/ex48/lexicon.py
+++ Skeleton/ex48/lexicon.py
@@ -2,3 +2,14 @@ directions = ('north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', '
 verbs = ('go', 'stop', 'kill', 'eat', 'open')
 stop_words = ('the', 'in', 'of', 'from', 'at', 'it')
 nouns = ('door', 'bear', 'princess', 'cabinet')
+
+
+def get_tuple(word):
+    lowercased = word.lower()
+
+    if lowercased in directions:
+        return ('direction', lowercased)
+    elif lowercased in verbs:
+        return ('verb', lowercased)
+    elif lowercased in stop_words:
+        return ('stop', lowercased)
