7e60519b94ea503cc9337383f946a57d4d4a59d5
Anayatc <anayatc@gmail.com>
2017-05-04 09:17:28 +0100

    advanced user input

--- Skeleton/ex48/parser.py
+++ Skeleton/ex48/parser.py
@@ -1,12 +1,16 @@
 class ParserError(Exception):
     pass
 
-class Sentence(object):
 
-    def __init__(self, subject, verb, obj):
+class Sentence(object):
+    def __init__(self, subject, verb, number, object):
         self.subject = subject[1]
         self.verb = verb[1]
-        self.obj = obj[1]
+        self.number = number[1]
+        self.object = object[1]
+
+    def to_tuple(self):
+        return (self.subject, self.verb, self.number, self.object)
 
 
 def peek(word_list):
@@ -43,33 +47,44 @@ def parse_verb(word_list):
         raise ParserError("Expected a verb next.")
 
 
+def parse_number(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'number':
+        return match(word_list, 'number')
+    else:
+        return ('number', 1)
+
+
 def parse_object(word_list):
     skip(word_list, 'stop')
-    next_word = peek(word_list)
+    next = peek(word_list)
 
-    if next_word == 'noun':
+    if next == 'noun':
         return match(word_list, 'noun')
-    elif next_word == 'direction':
+    elif next == 'direction':
         return match(word_list, 'direction')
     else:
         raise ParserError("Expected a noun or direction next.")
 
 
-def parse_subject(word_list):
-    skip(word_list, 'stop')
-    next_word = peek(word_list)
+def parse_subject(word_list, subj):
+    verb = parse_verb(word_list)
+    number = parse_number(word_list)
+    obj = parse_object(word_list)
 
-    if next_word == 'noun':
-        return match(word_list, 'noun')
-    elif next_word == 'verb':
-        return ('noun', 'player')
-    else:
-        raise ParserError("Expected a verb next.")
+    return Sentence(subj, verb, number, obj)
 
 
 def parse_sentence(word_list):
-    subj = parse_subject(word_list)
-    verb = parse_verb(word_list)
-    obj = parse_object(word_list)
+    skip(word_list, 'stop')
 
-    return Sentence(subj, verb, obj)
+    start = peek(word_list)
+
+    if start == 'noun':
+        subj = match(word_list, 'noun')
+        return parse_subject(word_list, subj)
+    elif start == 'verb':
+        return parse_subject(word_list, ('noun', 'player'))
+    else:
+        raise ParserError("Must start with noun or verb, not: %s" % start)
