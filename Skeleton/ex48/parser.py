e8e5d2aa5a4e50c84e7c668285264a984f692db1
Anayatc <anayatc@gmail.com>
2017-05-04 08:33:02 +0100

    advanced user input

--- Skeleton/ex48/parser.py
+++ Skeleton/ex48/parser.py
@@ -1,6 +1,16 @@
+from ex48 import lexicon
+
+
 class Sentence(object):
 
     def __init__(self, subject, verb, obj):
         self.subject = subject[1]
         self.verb = verb[1]
         self.obj = obj[1]
+
+    def peek(word_list):
+        if word_list:
+            word = word_list[0]
+            return word[0]
+        else:
+            return None
