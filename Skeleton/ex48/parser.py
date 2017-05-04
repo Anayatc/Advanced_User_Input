b5f87922c49f902e7eefcd63bc14ccbe6b7ae73a
Anayatc <anayatc@gmail.com>
2017-05-04 08:40:40 +0100

    advanced user input

--- Skeleton/ex48/parser.py
+++ Skeleton/ex48/parser.py
@@ -8,20 +8,22 @@ class Sentence(object):
         self.verb = verb[1]
         self.obj = obj[1]
 
-    def peek(word_list):
-        if word_list:
-            word = word_list[0]
-            return word[0]
+
+def peek(word_list):
+    if word_list:
+        word = word_list[0]
+        return word[0]
+    else:
+        return None
+
+
+def match(word_list, expecting):
+    if word_list:
+        word = word_list.pop(0)
+
+        if word[0] == expecting:
+            return word
         else:
             return None
-
-    def match(word_list, expecting):
-        if word_list:
-            word = word_list.pop(0)
-
-            if word[0] == expecting:
-                return word
-            else:
-                return None
-        else:
-            return None
+    else:
+        return None
