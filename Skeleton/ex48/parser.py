17907c7628af2a1cf57e73f3d6b7e745ef10e92a
Anayatc <anayatc@gmail.com>
2017-05-04 09:07:27 +0100

    advanced user input

--- Skeleton/ex48/parser.py
+++ Skeleton/ex48/parser.py
@@ -35,6 +35,15 @@ def skip(word_list, word_type):
 
 def parse_verb(word_list):
     skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb next.")
+
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
     next_word = peek(word_list)
 
     if next_word == 'noun':
@@ -52,7 +61,7 @@ def parse_subject(word_list):
     if next_word == 'noun':
         return match(word_list, 'noun')
     elif next_word == 'verb':
-        return ('noun', 'verb')
+        return ('noun', 'player')
     else:
         raise ParserError("Expected a verb next.")
 
@@ -60,4 +69,6 @@ def parse_subject(word_list):
 def parse_sentence(word_list):
     subj = parse_subject(word_list)
     verb = parse_verb(word_list)
-    obj = parse
+    obj = parse_object(word_list)
+
+    return Sentence(subj, verb, obj)
