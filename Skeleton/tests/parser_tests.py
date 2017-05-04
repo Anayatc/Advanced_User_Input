5b23ae017c9f1443444b0cebb3606e5758364078
Anayatc <anayatc@gmail.com>
2017-05-04 09:11:13 +0100

    advanced user input

--- Skeleton/tests/parser_tests.py
+++ Skeleton/tests/parser_tests.py
@@ -3,3 +3,10 @@ from ex48 import lexicon
 from ex48 import parser
 
 
+def test_sentence_obj():
+    s = parser.Sentence(('noun', 'bear'), ('verb', 'eat'), ('number', 1), ('noun', 'door'))
+    assert_equal(s.subject, 'bear')
+    assert_equal(s.verb, 'eat')
+    assert_equal(s.number, 1)
+    assert_equal(s.object, 'door')
+    assert_equal(s.to_tuple(), ('bear', 'eat', 1, 'door'))
